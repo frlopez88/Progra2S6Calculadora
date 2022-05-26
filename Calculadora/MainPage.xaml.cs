d1d23d53aa5c61020f1a0600ba53f8ad7ee097bf
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-25 20:49:32 -0600

    Configuracion de Numeros y Operadores

--- Calculadora/MainPage.xaml.cs
+++ Calculadora/MainPage.xaml.cs
@@ -10,9 +10,103 @@ namespace Calculadora
 {
     public partial class MainPage : ContentPage
     {
+        private double numero1 = 0;
+        private double numero2 = 0;
+        private string operacion = "";
+
         public MainPage()
         {
             InitializeComponent();
+
         }
+
+
+        void ClicAc(System.Object sender, System.EventArgs e) {
+            Resultados.Text = "";
+        }
+        void ClicBoton7(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            if (str == "0") {
+                str = "7";
+            }
+            else
+            {
+                str += "7";
+            }
+            Resultados.Text = str;
+        }
+
+        void ClicBoton8(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            if (str == "0")
+            {
+                str = "8";
+            }
+            else
+            {
+                str += "8";
+            }
+            Resultados.Text = str;
+        }
+
+        void ClicBoton9(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            if (str == "0")
+            {
+                str = "9";
+            }
+            else
+            {
+                str += "9";
+            }
+            Resultados.Text = str;
+        }
+
+        void ClicBotonMas(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            numero1 = Double.Parse(str);
+            operacion = "+";
+            Resultados.Text="";
+
+        }
+
+
+        void ClicBotonMenos(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            numero1 = Double.Parse(str);
+            operacion = "-";
+            Resultados.Text = "";
+
+        }
+
+        void ClicBotonIgual(System.Object sender, System.EventArgs e)
+        {
+            string str = Resultados.Text;
+            numero2 = Double.Parse(str);
+            double resultcalculo=0;
+
+            switch (operacion) {
+
+                case "+":
+                    resultcalculo = numero1 + numero2;
+                    break;
+                case "-":
+                    resultcalculo = numero1 - numero2;
+                    break;
+                default:
+                    break;
+
+            }
+
+            Resultados.Text = resultcalculo.ToString();
+
+
+        }
+
     }
 }
